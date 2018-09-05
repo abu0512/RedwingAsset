b09a9923fe9ce317372510183279870f7a8252c5
JJW <light_con@naver.com>
2018-09-05 21:35:49 +0900

    연동 했는데 커밋 하는 꺼 깜빡하고 작업하고 있었음 >ㅅ< !!

--- Script/UI/LobbyScene.cs
+++ Script/UI/LobbyScene.cs
@@ -2,20 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LobbyScene : MonoBehaviour {
 
-	public void AnyKeyDown()
+    public Image fade;
+    float fades = 1f;
+    float time = 0;
+    bool fadeinout = false;
+
+    private void FadeSet()
+    {
+      if(fadeinout)
+        {
+            Fadeout();
+        }
+
+      else if (!fadeinout)
+        {
+            Fadein();
+        }
+    }
+
+    private void Fadein()
+    {
+        time += Time.deltaTime;
+        if(fades > 0.0f && time >= 0.1f)
+        {
+            fades -= 0.2f;
+            fade.color = new Color(0, 0, 0, fades);
+            time = 0;
+        }
+
+        else if (fades <= 0.0f)
+        {
+            fadeinout = !fadeinout;
+        }
+    }
+
+    private void Fadeout()
+    {
+        time += Time.deltaTime;
+        if (fades < 1f && time >= 0.1f)
+        {
+            fades += 0.15f;
+            fade.color = new Color(0, 0, 0, fades);
+            time = 0;
+        }
+
+        else if (fades >= 1f)
+        {
+            fadeinout = !fadeinout;
+        }
+    }
+
+    public void AnyKeyDown()
     {
         if (Input.anyKey)
         {
-            SceneManager.LoadSceneAsync("NYR(MergeOnly)");
+            SceneManager.LoadSceneAsync("LoaddingScene");
         }
-
     }
 	
-	// Update is called once per frame
 	void Update () {
+        FadeSet();
         AnyKeyDown();
     }
 }
--- Script/UI/UiMaanger.cs
+++ Script/UI/UiMaanger.cs
@@ -22,6 +22,6 @@ public class UiMaanger : MonoBehaviour
 	
 	void Update ()
     {
-        BossParamsSet();
+        //BossParamsSet();
     }
 }
